f8e74a62320c46a4b59fa1f73d8f571fed34325d
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-03-24 18:31:06 -0400

    Created conntection to MySQL db.  Started Inquirer function with questions to trigger functions based on choices

--- server.js
+++ server.js
@@ -1,13 +1,76 @@
 //psuedo coding
 // import required modules
-const mysql =require("mysql");
+const mysql = require("mysql");
 const inquirer = require("inquirer");
 const conTable = require("console.table") //https://www.npmjs.com/package/console.table?activeTab=versions
 
 // start connection to MySql DB
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'test123',
+        database: 'employeeDB'
+    },
+
+);
+db.connect(function (err) {
+    if (err) throw err;
+    console.log(`Connected to the employeeDB database.`)
+    start();
+});
 
 
-// various functions
+
+// various functions 
+// function for inquirer questions to trigger functions based on choices
+
+function Start() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "start",
+            choices: [
+                "View all Employees",
+                "Add Employee",
+                "Remove Employee",
+                "View all Departments",
+                "Add Department",
+                "View all Roles",
+                "Add Roles",
+                "Update Employee Role",
+                "Exit"
+            ]
+        }
+    ])
+    .then(function(res) {
+        switch (res.start) {
+            
+
+
+
+
+        }
+
+    }
+
+
+
+
+
+        }
+    ])
+}
+
+
+
+
+
+
+
+
+
 // function to view employees
 // function to add employees
 // function to remove employees
