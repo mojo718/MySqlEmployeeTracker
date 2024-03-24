85d0b8fd0519ec5ca9362e0f826bdb4ef28e6e93
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-03-24 14:06:10 -0400

    Start code - mostly psuedo coding/Folder structure

---
+++ db/schema.sql
@@ -0,0 +1,1 @@
+future schema for my DB
---
+++ server.js
@@ -0,0 +1,18 @@
+//psuedo coding
+// import required modules
+
+// start connection to MySql DB
+
+// various functions
+// function to view employees
+// function to add employees
+// function to remove employees
+
+//function to view departments
+// function to add departments
+// function to remove departments
+
+//function to view roles
+// function to add roles
+// function to remove roles
+
