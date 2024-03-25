d2caa17d79b80601f21db81445e26c4dbef7d3d5
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-03-24 20:13:10 -0400

    Added functions to view/add/remove emplyees

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ db.connect(function (err) {
 // various functions 
 // function for inquirer questions to trigger functions based on choices
 
-function Start() {
+function start() {
     inquirer.prompt([
         {
             type: "list",
@@ -44,36 +44,129 @@ function Start() {
             ]
         }
     ])
-    .then(function(res) {
-        switch (res.start) {
-            
+        .then(function (res) {
+            switch (res.start) {
 
+                case "Add Employee":
+                    addEmployee();
+                    break;
 
+                case "View all Employees":
+                    viewAllEmployees();
+                    break;
 
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
 
-        }
+                case "Add Department":
+                    addDept();
+                    break;
 
-    }
+                case "View all Departments":
+                    viewAllDept();
+                    break;
 
+                case "Add Roles":
+                    addRole();
+                    break;
 
+                case "View all Roles":
+                    viewAllRoles();
+                    break;
 
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
 
-
-        }
-    ])
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        })
 }
 
-
-
-
-
-
-
-
-
 // function to view employees
+function viewAllEmployees() {
+
+    connection.query("SELECT employees.first_name, employees.last_name, roles.title AS \"role\", managers.first_name AS \"manager\" FROM employees LEFT JOIN role ON employees.role_id = role.id LEFT JOIN employees managers ON employees.manager_id = managers.id GROUP BY employees.id",
+        function (err, res) {
+            if (err) throw err;
+            // Log all results of the SELECT statement
+            console.table(res);
+            start();
+        });
+}
 // function to add employees
+function addEmployee() {
+    console.log("Inserting a new employee.\n");
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "First Name?",
+                name: "first_name",
+            },
+            {
+                type: "input",
+                message: "Last Name?",
+                name: "last_name"
+            },
+            {
+                type: "list",
+                message: "What is the employee's role?",
+                name: "role_id",
+                choices: [1, 2, 3]
+            },
+            {
+                type: "input",
+                message: "Who is their manager?",
+                name: "manager_id"
+            }
+        ])
+        .then(function (res) {
+            const query = connection.query(
+                "INSERT INTO employee SET ?",
+                res,
+                function (err, res) {
+                    if (err) throw err;
+                    console.log("Employee added!\n");
+
+                    start();
+                }
+            );
+        })
+}
 // function to remove employees
+function removeEmployee() {
+    let employeeList = [];
+    connection.query(
+      "SELECT employees.first_name, employees.last_name FROM employees", (err, res) => {
+        for (let i = 0; i < res.length; i++) {
+          employeeList.push(res[i].first_name + " " + res[i].last_name);
+        }
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              message: "Which employee would you like to delete?",
+              name: "employee",
+              choices: employeeList
+  
+            },
+          ])
+          .then(function (res) {
+            const query = connection.query(
+              `DELETE FROM employees WHERE concat(first_name, ' ' ,last_name) = '${res.employee}'`,
+              function (err, res) {
+                if (err) throw err;
+                console.log("Employee deleted!\n");
+                start();
+              });
+          });
+      }
+    );
+  };
 
 //function to view departments
 // function to add departments
